eea756669db4c8cdf553c7fd189e0e2debb222a2
Programmer Astolfo <programmerastolfo@gmail.com>
2023-10-05 21:45:02 +0200

    regex pain v3

--- cogs/nya_channel_limit.py
+++ cogs/nya_channel_limit.py
@@ -16,8 +16,8 @@ class NyaChannelLimit(discord.Cog):
             return
         if msg.channel.id != constants.nya_channel:
             return
-        regex_1 = re.search(r'^[Nn][y]+[a]+[~]?(\s+[;:][3]+)?$', msg.clean_content)
-        regex_2 = re.search(r'^[Mm][er]+[o]?[w]?[~]?(\s+[;:][3])?$', msg.clean_content)
+        regex_1 = re.search(r'^[Nn][y]+[a]+[h]*[~]?(\s+[;:][3]+)?$', msg.clean_content)
+        regex_2 = re.search(r'^[Mm][e]+[r]+[o]?[w]?[~]?(\s+[;:][3])?$', msg.clean_content)
         if regex_1 is None and regex_2 is None:
             await msg.delete()
             await msg.channel.send(f'{msg.author.mention} This channel is only for meowing, nothing else.', delete_after=5)
