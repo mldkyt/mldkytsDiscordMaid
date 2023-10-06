dc39617d504e20ebdb8a936aae98f03a75f5f42b
Programmer Astolfo <programmerastolfo@gmail.com>
2023-10-06 20:36:23 +0200

    regex 4 i am losing brain cells

--- cogs/nya_channel_limit.py
+++ cogs/nya_channel_limit.py
@@ -17,7 +17,7 @@ class NyaChannelLimit(discord.Cog):
         if msg.channel.id != constants.nya_channel:
             return
         regex_1 = re.search(r'^[Nn][y]+[a]+[h]*[~]?(\s+[;:][3]+)?$', msg.clean_content)
-        regex_2 = re.search(r'^[Mm][e]+[r]+[o]?[w]?[~]?(\s+[;:][3])?$', msg.clean_content)
+        regex_2 = re.search(r'^[Mm][er]+[o]?[w]?[~]?(\s+[;:][3])?$', msg.clean_content)
         if regex_1 is None and regex_2 is None:
             await msg.delete()
             await msg.channel.send(f'{msg.author.mention} This channel is only for meowing, nothing else.', delete_after=5)
